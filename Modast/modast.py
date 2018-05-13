4b5068e9ff2c2e732f7b4c50c77f443e5be5259b
Cristian Weiland <cw14@inf.ufpr.br>
2018-05-13 14:47:04 -0300

    Add plot option
    Signed-off-by: Cristian Weiland <cw14@inf.ufpr.br>

--- Modast/modast.py
+++ Modast/modast.py
@@ -1,8 +1,13 @@
+# Libraries
 import json
+import time
 import argparse
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+# Local Imports
 import variables # ./variables.py
 import states # ./states.py
-from pprint import pprint
 
 parser = argparse.ArgumentParser(description='Modast in python.')
 
@@ -12,8 +17,16 @@ parser.add_argument('-nt', '--normalize', action="store", dest="normalize",
             help="[str true/false], if we should or not normalize time", default="true")
 parser.add_argument('-w', '--window', action="store", dest="windowSize",
             help="[int] window size, -1 = infinity", default="-1")
+parser.add_argument('-ppt', '--printperformancetrend', action="store", dest="ppt",
+            help="[str true/false] debug performance trend or not", default="false")
+parser.add_argument('-plot', '--plot', action="store", dest="plot",
+            help="[str true/false] plot results", default="true")
+parser.add_argument('-separate', '--separatedwindows', action="store", dest="separate",
+            help="[str true/false] separate into 4 graph windows", default="false")
 
 args = parser.parse_args()
+
+ppt = args.ppt == 'true'
 
 with open(args.filename) as f:
     data = json.load(f)
@@ -54,6 +67,11 @@ currentState = states.initialState
 
 # We will simulate receiving data here. So, we will calculate states using
 # arrays of length 1, length 2, ..., length len(timeSorted)
+finalPV = []
+finalTT = []
+finalPT = []
+finalCS = []
+xPlot = []
 for i in range(0, len(timeSorted)):
     # First, slice arrays so we dont have data from the future
     partialTime = timeSorted[:i+1]
@@ -63,9 +81,15 @@ for i in range(0, len(timeSorted)):
     performanceVariation = variables.calculatePerformanceVariation(i, partialTreated, windowSize)
     # transactionTroughput only looks to current second, so we dont need windowSize
     transactionTroughput = variables.calculateTransactionTroughput(partialTreated[i], partialRequested[i])
-    performanceTrend = variables.calculatePerformanceTrend(partialTime, partialTreated, windowSize)
+    performanceTrend = variables.calculatePerformanceTrend(partialTime, partialTreated, windowSize, ppt)
 
     currentState = states.getNextState(currentState, performanceVariation, transactionTroughput, performanceTrend, partialTreated[i])
+
+    finalPV.append(performanceVariation)
+    finalTT.append(transactionTroughput)
+    finalPT.append(performanceTrend)
+    finalCS.append(currentState)
+    xPlot.append(i)
 
     print('+-----------------------------------------------------------+')
     print('| Iteration #' + str(i) + "\n|")
@@ -74,6 +98,55 @@ for i in range(0, len(timeSorted)):
     print('| Performance Trend: ' + str(performanceTrend))
     print('| Current State: ' + str(currentState))
     print('+-----------------------------------------------------------+\n')
+
+def plot(x, y, label, title, args):
+    plt.plot(x, y)
+    plt.xlabel('Time (Seconds)')
+    plt.ylabel(label)
+    plt.title(title)
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
+    if (args.separate == 'true'):
+        plt.show()
+
+def parseState(state):
+    if (state == 'warmup'):
+        return 0
+    if (state == 'steady'):
+        return 1
+    if (state == 'under_pressure'):
+        return 2
+    if (state == 'stress'):
+        return 3
+    if (state == 'thrashing'):
+        return 4
+    return -1
+
+if args.plot == 'true':
+    # Subplot separates the plot window into a grid. 221 means ( 1 2 )
+    # grid size is 2x2, and 1 means its on the first position: ( 3 4 )
+    if args.separate != 'true':
+        plt.subplot(221)
+    plot(xPlot, finalPV, 'Performance Variation', 'Performance Variation x Time', args)
+    if args.separate != 'true':
+        plt.subplot(222)
+    plot(xPlot, finalTT, 'Transaction Troughput', 'Transaction Troughput x Time', args)
+    if args.separate != 'true':
+        plt.subplot(223)
+    plot(xPlot, finalPT, 'Performance Trend', 'Performance Trend x Time', args)
+    finalCS = [parseState(x) for x in finalCS]
+    if args.separate != 'true':
+        plt.subplot(224)
+    plt.plot(xPlot, finalCS)
+    plt.xlabel('Time (Seconds)')
+    plt.ylabel('Current State')
+    plt.title('Current State x Time')
+    x1,x2,y1,y2 = plt.axis()
+    plt.axis((x1,x2,-1,5))
+
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
+    plt.show()
 
 # performanceTrend = variables.calculatePerformanceTrend(timeSorted, treatedSorted)
 # print(performanceTrend)
--- Modast/variables.py
+++ Modast/variables.py
@@ -1,6 +1,6 @@
 import math
 import numpy as np
-    
+
 # Params:
 # - (Int) n: number of observations
 # - (Int array) nTransactionsTreated: Array of ints representing the number of treated transactions in that second;
@@ -32,22 +32,33 @@ def calculateTransactionTroughput(nTransactionsTreated, nTransactionsRequested):
 # Params:
 # - (Float array) x: Array containing time data (X axis)
 # - (Float array) y: Array containing transactionsTreated data (Y axis)
-def calculatePerformanceTrend(x, y, windowSize):
+def calculatePerformanceTrend(x, y, windowSize, debug):
     if len(x) != len(y):
         print('Calculating performanceTrend with two arrays of different size... Aborting...')
         return
 
     size = min(windowSize, len(x))
 
+    if debug:
+        print('Size is ' + str(size))
+
     # array[-size:] gives us the last <size> elements of the array
     npx = np.array(x[-size:])
     npy = np.array(y[-size:])
+
+    if debug:
+        print('Npx size ' + str(len(npx)))
+        print('Npy size ' + str(len(npy)))
 
     # Use least squares to find (a, b) so that f(x) = ax + b. Returns array [a,b].
     z = np.polyfit(npx, npy, 1)
     # Create function in python that implements the f(x) obtained in the last command
     fx = np.poly1d(z) # now, if i want to calculate where x = 0.5, just do fx(0.5)
-    #print("f(x) = (" + str(z[0]) + ") * x + (" + str(z[1]) + ")")
+
+    if debug:
+        print("f(x) = (" + str(z[0]) + ") * x + (" + str(z[1]) + ")")
+
+
 
     # Print correct values vs generated function
     # for i in range(0, len(x)):
@@ -58,13 +69,19 @@ def calculatePerformanceTrend(x, y, windowSize):
     if decrescent:
         root = fx.r # calculate x'
 
+        if debug:
+            print('Decrescent, root is ' + str(root) + '\n')
+
         # print("its 0 when...")
         # print(root)
         # print(fx(root))
 
         return root - x[len(x)-1]
 
-    return None 
+    if debug:
+        print('Crescent function.\n')
+
+    return None
 
 # Construct the polynomial x^2 + 2x + 3:
 
